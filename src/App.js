0a04d809d1b98b2ce5af6ebf2b95f49b797ebb5f
gustavogds <gustavogds142000@gmail.com>
2023-01-25 15:24:14 -0300

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/home/Home';
+import { Skills } from './pages/skills/Skills';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Working in progress...
-      </header>
+      <Router>
+        <NavBar />
+        <div className="App-body">
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/skills" element={<Skills />} />
+            
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/skills/Skills.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Skills.css'
+
+export const Skills = () => {
+    return (
+        <div>
+            <h1>Skills</h1>
+        </div>
+    );
+}
